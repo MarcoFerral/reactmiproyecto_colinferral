1899199531d49911573ea783010453e4fddf9655
Marco Ferral <marcoferral@hotmail.com>
2022-08-17 23:59:10 -0500

    primer commit ensayo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <>
+    <h1>Hola Mundo!!</h1>
+    <h3>Ya estoy aquí de nuevo</h3>
+    <li>Usar la metodología de React</li>
+    <li>Aprender algo nuevo</li>
+    </>
+  );
+}
+
+export default App;
