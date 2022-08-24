5b25df77d9caad08f96990875afea6718259ae16
Marco Ferral <marcoferral@hotmail.com>
2022-08-23 21:10:21 -0500

    tercer commit, agregados algunos componentes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 
 function App() {
   const mensaje = 'Bienvenido a mi App'
-
+  const estilos = {
+    backgroundColor:'#888',
+    padding: '20px'
+  }
 
   return (
   <div className= 'container'>
     <h1>Hola Mundo!!</h1>
     <h3>Ya estoy aquí de nuevo</h3>
-    <h2 style={ {backgroundColor:'#888'} }>{ mensaje }</h2>
+    <h2 style={ estilos }>{ mensaje }</h2>
   </div>
   );
 }
