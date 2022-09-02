bbcba9b7ffc7622c6cacfbe5bd5db0d5c24aad85
Marco Ferral <marcoferral@hotmail.com>
2022-09-02 11:36:28 -0500

    ultimos detalles de useState

--- src/App.js
+++ src/App.js
@@ -3,14 +3,29 @@ import NavBar2 from './components/NavBar2';
 import Bienvenida from './components/Bienvenida';
 import Login from './Login';
 import ItemListContainer from './components/ItemListContainer';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [counter, setCounter] = useState(0)
+  
+  useEffect( () => {
+    console.log('efecto al montaje');
+  }, [])
+
+  useEffect( () => {
+    console.log('efecto al montaje y cambio en counter');
+  }, [counter])
+
+  useEffect( () => {
+    console.log('efecto en cada render');
+  })
+
   const handleClick = () => {
     console.log('hiciste click');
     setCounter( counter + 1 )
   }
+
+  console.log('hubo render');
 
   return (
   <div className= 'container'>
