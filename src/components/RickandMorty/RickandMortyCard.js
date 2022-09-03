2955c2b3943dd40a728af6ee4071329033d429a3
Marco Ferral <marcoferral@hotmail.com>
2022-09-03 02:07:52 -0500

    inclusión de cards al item list

--- src/components/RickandMorty/RickandMortyCard.js
+++ src/components/RickandMorty/RickandMortyCard.js
@@ -1,9 +1,20 @@
 const RickandMortyCard = ( {id, name, species, image} ) => {
   return (
     <div>
-        <h1>{id}-{name}</h1>
+      <div class="card w-96 bg-base-100 shadow-xl">
+  <figure class="px-10 pt-10">
+    <img src={image} alt="Shoes" class="rounded-xl" />
+  </figure>
+  <div class="card-body items-center text-center">
+    <h2 class="card-title">{name}</h2>
+    <p>{species}</p>
+    <div class="card-actions">
+    </div>
+  </div>
+</div>
+        {/* <h1>{id}-{name}</h1>
         <h3>{species}</h3>
-        <img src={image}/>
+        <img src={image}/> */}
     </div>
   )
 }
--- src/components/RickandMorty/RickandMortyCardList.js
+++ src/components/RickandMorty/RickandMortyCardList.js
@@ -3,7 +3,7 @@ import RickandMortyCard from "./RickandMortyCard"
 const RickandMortyCardList = ({chars}) => {
   return (
     <div>
-{/* { chars.map( c => <RickandMortyCard key={c.id}{...c}/>) }*/}    
+    {/* { chars.map( c => <RickandMortyCard key={c.id}{...c}/>) }*/}    
     { chars.map( c => 
         <RickandMortyCard key={c.id} id={c.id}name={c.name} species={c.species} image={c.image}/>
         )}    
