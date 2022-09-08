6be8fdf067661746b05944a2b1b6342372268c84
Marco Ferral <marcoferral@hotmail.com>
2022-09-07 22:42:36 -0500

    Estableciendo routes de las 3 apps creadas en index.js

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ import reportWebVitals from './reportWebVitals';
 import RMortyApp from './RMortyApp';
 import App from './App';
 import SwapiContainer from './components/swapi/SwapiContainer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* //<App />
-    //<RMortyApp/> */}
-    <SwapiContainer/>
-  </React.StrictMode>
+  //<React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+        <Route path='/swapi' element={<SwapiContainer/>}/>
+        <Route path='/' element={<RMortyApp/>}/>
+      </Routes>
+    </BrowserRouter>
+  //</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
